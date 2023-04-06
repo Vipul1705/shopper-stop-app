c4db02b3d31e0a6950bf7e1869d3c86d6933d0a3
Vipul Patil <61792938+Vipul1705@users.noreply.github.com>
2023-04-06 11:55:12 +0530

    fixed bugs

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,26 +2,26 @@ import { useState, useEffect } from "react";
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
-const DUMMY_PRODUCTS = [
-  {
-    id: "p1",
-    price: 6,
-    title: "My first Book",
-    description: "My first book ever written",
-  },
-  {
-    id: "p2",
-    price: 10,
-    title: "My 2nd Book",
-    description: "My 2nd book ever written",
-  },
-  {
-    id: "p3",
-    price: 20,
-    title: "My 3rd Book",
-    description: "My 3rd book ever written",
-  },
-];
+// const DUMMY_PRODUCTS = [
+//   {
+//     id: "p1",
+//     price: 6,
+//     title: "My first Book",
+//     description: "My first book ever written",
+//   },
+//   {
+//     id: "p2",
+//     price: 10,
+//     title: "My 2nd Book",
+//     description: "My 2nd book ever written",
+//   },
+//   {
+//     id: "p3",
+//     price: 20,
+//     title: "My 3rd Book",
+//     description: "My 3rd book ever written",
+//   },
+// ];
 
 const Products = (props) => {
   const [products, setProducts] = useState([]);
